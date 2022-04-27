b5c1f43cceba4d8f72b8c859ade01cdbbef4aa99
Tylar Murray <mail@tylar.info>
2022-04-27 15:51:09 -0400

    + na filter. + tiny region data: fk.

--- data-raw/occ.R
+++ data-raw/occ.R
@@ -1,12 +1,17 @@
+# This file is used to generate subsets of data from the full OBIS .parquet export.
+#
 # occ ----
 librarian::shelf(
   arrow, dplyr, here, readr)
 
 # obis_20220404.parquet downloaded from https://obis.org/data/access on 2022-04-26
-occ <- open_dataset(here("data/obis_20220404.parquet"))
-occ <- occ %>%
-  select(decimalLongitude, decimalLatitude, species) %>%
-  group_by(decimalLongitude, decimalLatitude, species) %>%
+occ <- open_dataset("../../obis_20220404.parquet")
+
+# NOTE: there are lots of other fields in the parquet file.
+#     These could be used in the future.
+trimmed_occ <- select(occ, decimalLongitude, decimalLatitude, species) %>%    # drop unnecessary columns
+  group_by(decimalLongitude, decimalLatitude, species) %>%  # remove dulplicate rows
+  filter(!is.na(species))  %>%
   collect() %>%
   summarize(records = n()) %>%
   ungroup()
@@ -14,14 +19,24 @@ occ <- occ %>%
 # occ_1M, subsampled global occurrence dataset ----
 set.seed(42)
 i <- sample(1:nrow(occ), 1000000)
-occ_1M <- slice(pcc, i)
-usethis::use_data(occ_1M)
+occ_1M <- slice(trimmed_occ, i)
+usethis::use_data(occ_1M, overwrite=TRUE)
 
 # occ_SAtlantic,full regional occurrence dataset ----
 # Southern Ocean chosen since somewhat sparsely populated and provides a latitudinal gradient
 # [Marine Regions · South Atlantic Ocean (IHO Sea Area)](https://marineregions.org/gazetteer.php?p=details&id=1914)
-occ_SAtlantic <- occ %>%
+occ_SAtlantic <- trimmed_occ %>%
   filter(
     decimalLatitude  >= -60     , decimalLatitude  <= 0.0751,
     decimalLongitude >= -69.6008, decimalLongitude <= 20.0091) # 1,014,006 × 4
-usethis::use_data(occ_SAtlantic)
+usethis::use_data(occ_SAtlantic, overwrite=TRUE)
+
+# occ_fk,full regional occurrence dataset ----
+# Tiny section around the FL keys
+occ_fk <- trimmed_occ %>%
+  filter(
+    decimalLatitude <= -70,
+    decimalLatitude >= -90,
+    decimalLongitude >= 20,
+    decimalLongitude <= 30)
+usethis::use_data(occ_fk, overwrite=TRUE)
