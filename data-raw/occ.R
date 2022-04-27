b44c6c5d91ff18280907cf04dd5244069bd1a9d0
Tylar Murray <mail@tylar.info>
2022-04-27 18:04:36 -0400

    + ts data subsets

--- data-raw/occ.R
+++ data-raw/occ.R
@@ -1,4 +1,5 @@
 # This file is used to generate subsets of data from the full OBIS .parquet export.
+# This file will take a long time to run, so press go and go get a snack.
 #
 # occ ----
 librarian::shelf(
@@ -9,8 +10,8 @@ occ <- open_dataset("../../obis_20220404.parquet")
 
 # NOTE: there are lots of other fields in the parquet file.
 #     These could be used in the future.
-trimmed_occ <- select(occ, decimalLongitude, decimalLatitude, species) %>%    # drop unnecessary columns
-  group_by(decimalLongitude, decimalLatitude, species) %>%  # remove dulplicate rows
+trimmed_occ <- select(occ, decimalLongitude, decimalLatitude, species, date_year) # drop unnecessary columns
+grouped_occ <- group_by(trimmed_occ, decimalLongitude, decimalLatitude, species, date_year) %>%  # remove dulplicate rows
   filter(!is.na(species))  %>%
   collect() %>%
   summarize(records = n()) %>%
@@ -19,13 +20,13 @@ trimmed_occ <- select(occ, decimalLongitude, decimalLatitude, species) %>%    # 
 # occ_1M, subsampled global occurrence dataset ----
 set.seed(42)
 i <- sample(1:nrow(occ), 1000000)
-occ_1M <- slice(trimmed_occ, i)
+occ_1M <- slice(grouped_occ, i)
 usethis::use_data(occ_1M, overwrite=TRUE)
 
 # occ_SAtlantic,full regional occurrence dataset ----
 # Southern Ocean chosen since somewhat sparsely populated and provides a latitudinal gradient
 # [Marine Regions · South Atlantic Ocean (IHO Sea Area)](https://marineregions.org/gazetteer.php?p=details&id=1914)
-occ_SAtlantic <- trimmed_occ %>%
+occ_SAtlantic <- grouped_occ %>%
   filter(
     decimalLatitude  >= -60     , decimalLatitude  <= 0.0751,
     decimalLongitude >= -69.6008, decimalLongitude <= 20.0091) # 1,014,006 × 4
@@ -33,10 +34,59 @@ usethis::use_data(occ_SAtlantic, overwrite=TRUE)
 
 # occ_fk,full regional occurrence dataset ----
 # Tiny section around the FL keys
-occ_fk <- trimmed_occ %>%
+occ_fk <- grouped_occ %>%
   filter(
     decimalLatitude <= -70,
     decimalLatitude >= -90,
     decimalLongitude >= 20,
     decimalLongitude <= 30)
 usethis::use_data(occ_fk, overwrite=TRUE)
+
+# time series ----
+oc_ts <- c(
+    filter(
+      grouped_occ,
+      date_year <= 2020,
+      date_year >= 2010
+    ),
+    filter(
+      grouped_occ,
+      date_year <= 2010,
+      date_year >= 2000
+    ),
+    filter(
+      grouped_occ,
+      date_year <= 2000,
+      date_year >= 1990
+    ),
+    filter(
+      grouped_occ,
+      date_year <= 1990,
+      date_year >= 1980
+    ),
+    filter(
+      grouped_occ,
+      date_year <= 1980,
+      date_year >= 1970
+    ),
+    filter(
+      grouped_occ,
+      date_year <= 1980,
+      date_year >= 1970
+    ),
+    filter(
+      grouped_occ,
+      date_year <= 1970,
+      date_year >= 1960
+    )
+)
+
+min_n_samples <- min(oc_ts)
+
+usethis::use_data(occ_2010s, overwrite=TRUE)
+usethis::use_data(occ_2000s, overwrite=TRUE)
+usethis::use_data(occ_1990s, overwrite=TRUE)
+usethis::use_data(occ_1980s, overwrite=TRUE)
+usethis::use_data(occ_1970s, overwrite=TRUE)
+usethis::use_data(occ_1960s, overwrite=TRUE)
+
